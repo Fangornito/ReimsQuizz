6ec6e4e2943ce78341561406c8ff6a1630bccb93
herimanana301 <101905769+herimanana301@users.noreply.github.com>
2022-10-06 16:49:42 +0200

    scrpit of benoit

--- src/script.js
+++ src/script.js
@@ -1,21 +1,9 @@
-let points = document.querySelector('.score').innerHTML
-let message = document.createElement('h1')
-let parent = document.querySelector("#page5")
-
-//switch (points){
-   // case (1):
-       // message.innerHTML="Pittoyable..."
-        //parent.appendChild(message)
-    //case (2):
-        //message.innerHTML="Tu peux mieu faire"
-        //parent.appendChild(message)
-    //case (3):
-        //message.innerHTML="Bien !"
-        //parent.appendChild(message)
-
-    //case(4):
-        //message.innerHTML="Bien joué ! Un perfect !"
-        //parent.appendChild(message)
-//}
-
-console.log(points)
+const validButtons = document.querySelectorAll(".valid-button")
+validButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const inputs = document.querySelectorAll(`input[name=${button.dataset.input}`)
+        inputs.forEach((input) => {
+            input.classList.add(input.dataset.response);
+        })
+    })
+})
